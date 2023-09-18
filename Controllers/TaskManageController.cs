544a0bd759c670c03b17eb00f4f684fe3cecde18
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-09-17 22:59:29 -0500

    Add project files.

---
+++ Controllers/TaskManageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSwebPROJ1.Models;
+
+namespace MSwebPROJ1.Controllers
+{
+    public class TaskManageController : Controller
+    {
+
+        private List<TaskModel> tasks = new();
+        public IActionResult Index()
+        {
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add new task to the list
+                //(This would generally be saved to a database)
+                tasks.Add(task);
+
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+    }
+}
---
+++ Models/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿//This model represents the task data
+using System.ComponentModel.DataAnnotations;
+
+namespace MSwebPROJ1.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the Task Name.")]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Due Date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
